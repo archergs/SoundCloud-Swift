e4d6211fa51abc5792bd5245d3d8b07d373651f9
Archer Gardiner-Sheridan <30485025+archergs@users.noreply.github.com>
2024-02-05 13:28:26 +1030

    change name

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SoundCloud",
+    name: "SoundCloudSwift",
     defaultLocalization: "en",
     platforms: [.iOS(.v15), .watchOS(.v9), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SoundCloud",
-            targets: ["SoundCloud"]),
+            name: "SoundCloudSwift",
+            targets: ["SoundCloudSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SoundCloud",
+            name: "SoundCloudSwift",
             dependencies: [.product(name: "KeychainSwift", package: "keychain-swift")]),
         .testTarget(
-            name: "SoundCloudTests",
-            dependencies: ["SoundCloud"]),
+            name: "SoundCloudSwiftTests",
+            dependencies: ["SoundCloudSwift"]),
     ]
 )
